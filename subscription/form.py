90c78175d1560303acc39d7b0a8a712f4a4e976a
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-06-24 14:11:20 +0200

    pushing subscription app (test) with form.py based on model

--- subscription/form.py
+++ subscription/form.py
@@ -1,3 +1,32 @@
 from django import forms
 
 from .models import Subscription
+
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = [
+            'email',
+            'first_name',
+            'last_name',
+            'location',
+        ]
+
+        #alternatives : 
+        #fields = '__all__'
+        #exclude = ['timestamp'] (si fields n'est pas spécifié mais seulement exclude, alors fields par défaut vaut '__all__')
+
+        labels = {
+            'email': 'Addresse e-mail',
+            'first_name': 'Prénom',
+            'last_name': 'Nom',
+            'location': 'Commune',
+        }
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
